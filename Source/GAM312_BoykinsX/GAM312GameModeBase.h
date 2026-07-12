8be72f1b7bd8562b0387600d9394d06335c153e7
Xaviar Boykins <xaviarboykins@outlook.com>
2026-07-11 22:04:59 -0700

    Feat: Player Character setup and movement

---
+++ Source/GAM312_BoykinsX/GAM312GameModeBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "GAM312GameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GAM312_BOYKINSX_API AGAM312GameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
